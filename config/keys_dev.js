86f51c3778a396f2a85be141f4f52a9ef776b9f8
Lei <leichen83@gmail.com>
2019-04-19 22:04:55 -0400

    test

--- config/keys_dev.js
+++ config/keys_dev.js
@@ -1,4 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb://localhost:27017/criticsmentwork",
+  mongoURI: process.env.MONGO_URI ||"mongodb://localhost:27017/criticsmentwork",
   secretOrKey: "secret"
 };
